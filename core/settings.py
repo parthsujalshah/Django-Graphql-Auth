31bf5cc19ff60878c1c7ba4d9ed64d7e140f4975
parthsujalshah <parthsujalshah@gmail.com>
2021-07-30 21:24:23 +0530

    user register

--- core/settings.py
+++ core/settings.py
@@ -144,3 +144,13 @@ AUTHENTICATION_BACKENDS = [
     'graphql_auth.backends.GraphQLAuthBackend',
     'django.contrib.auth.backends.ModelBackend',
 ]
+
+GRAPHQL_JWT = {
+    'JWT_ALLOW_ANY_CLASSES': [
+        'graphql_auth.mutations.Register'
+    ],
+    'JWT_VERIFY_EXPIRATION': True,
+    'JWT_LONG_RUNNING_REFRESH_TOKEN': True
+}
+
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
--- users/schema.py
+++ users/schema.py
@@ -1,7 +1,14 @@
 import graphene
+from graphql_auth import mutations
 from graphql_auth.schema import UserQuery, MeQuery
+
+class AuthMutation(graphene.ObjectType):
+    register = mutations.Register.Field()
 
 class Query(UserQuery, MeQuery, graphene.ObjectType):
     pass
 
-schema = graphene.Schema(query=Query)
+class Mutation(AuthMutation, graphene.ObjectType):
+    pass
+
+schema = graphene.Schema(query=Query, mutation=Mutation)
