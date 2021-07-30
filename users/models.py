8f6a7abb2c242508e762e123d303302fbb32b419
parthsujalshah <parthsujalshah@gmail.com>
2021-07-30 18:56:16 +0530

    custom user

--- users/models.py
+++ users/models.py
@@ -1,3 +1,9 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+
+class ExtendedUser(AbstractUser):
+    email = models.EmailField(blank=False, max_length=255)
+
+    USERNAME_FIELD = 'username'
+    EMAIL_FIELD = 'email'   
